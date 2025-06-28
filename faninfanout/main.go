2b64d0e4f6011852b824f0ad039f0a93c9d94b53
jbdoumenjou <925513+jbdoumenjou@users.noreply.github.com>
2025-06-28 13:53:12 +0200

    with atomic counter

--- faninfanout/main.go
+++ faninfanout/main.go
@@ -5,40 +5,50 @@ import (
 	"log/slog"
 	"math/rand"
 	"sync"
+	"sync/atomic"
 	"time"
+)
+
+var (
+	totalUpdate atomic.Int64
 )
 
 type Order struct {
 	ID     int
 	Status string
+	mu     sync.Mutex
 }
 
 func main() {
 	startTime := time.Now()
 	defer func() {
-		slog.Info("All operations completed.", slog.String("uptime", time.Since(startTime).String()))
+		slog.Info("All operations completed.",
+			slog.String("uptime", time.Since(startTime).String()),
+			slog.Int64("total_updates", totalUpdate.Load()))
 	}()
 
 	wg := sync.WaitGroup{}
 	wg.Add(3)
 	orders := generateOrders(20)
 
-	go func() {
+	/*	go func() {
 		defer wg.Done()
 		processOrders(orders)
-	}()
+	}()*/
 
-	go func() {
-		defer wg.Done()
-		updateOrderStatuses(orders)
-	}()
+	for range 3 {
+		go func() {
+			defer wg.Done()
+			for _, order := range orders {
+				updateOrderStatus(order)
+			}
 
-	go func() {
-		defer wg.Done()
-		reportOrderStatuses(orders)
-	}()
+		}()
+	}
 
 	wg.Wait()
+
+	reportOrderStatuses(orders)
 }
 
 func updateOrderStatuses(orders []*Order) {
@@ -55,17 +65,28 @@ func updateOrderStatuses(orders []*Order) {
 	}
 }
 
-func reportOrderStatuses(orders []*Order) {
-	for i := range 5 {
-		time.Sleep(1 * time.Second) // Simulate periodic reporting
+func updateOrderStatus(order *Order) {
+	time.Sleep(time.Duration(rand.Intn(300)) * time.Millisecond) // Simulate processing time
 
-		fmt.Println("--Order Status Report --")
-		slog.Info("Order Status Report", slog.Int("iteration", i+1))
-		for _, order := range orders {
-			fmt.Println(fmt.Sprintf("Order %d - Status: %s", order.ID, order.Status))
-		}
-		fmt.Println("--End of Order Status Report --")
+	order.mu.Lock()
+	defer order.mu.Unlock()
+	status := []string{"Pending", "Processing", "Shipped", "Delivered"}[rand.Intn(4)]
+	order.Status = status
+
+	slog.Info("Updated Order Status",
+		slog.Int("orderID", order.ID),
+		slog.String("status", order.Status),
+	)
+
+	totalUpdate.Add(1)
+}
+
+func reportOrderStatuses(orders []*Order) {
+	fmt.Println("--Order Status Report --")
+	for _, order := range orders {
+		fmt.Println(fmt.Sprintf("Order %d - Status: %s", order.ID, order.Status))
 	}
+	fmt.Println("--End of Order Status Report --")
 }
 
 func processOrders(orders []*Order) {
